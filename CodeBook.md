b4b828879bd8c221d40ce039291281fe6ebea82a
Flavio Massimo Saldicco <f.saldicco@gmail.com>
2015-09-25 06:51:36 +0200

    Initial commit

---
+++ CodeBook.md
@@ -0,0 +1,91 @@
+=================
+This is a modified version of the original "features.txt" files distributed with the
+UCI HAR Dataset, introduced to fulfill the requirements of the Getting and Cleaning
+Data course Peer Assessment by Johns Hopkins Bloomberg School of Public Health on the 
+Coursera platform.
+Modification made by Flavio Massimo Saldicco on 24 Sep 2015.
+
+The original datasets have been transformed and two full and tidy datasets have been
+produced:
+
+* tstComplete - contains the full tidy test data, (2947 observations of 564 variables).
+* trnComplete - contains the full tidy train data (7352 observations of 564 variables)
+
+After merging this two data sets, a summarising data set has been produced:
+
+* sumDS - after merging tstComplete and trnComplete, the dataset have been grouped by subject code (sub_code) and activity description (act_descr); then the mean() of each measure (i.e., column) have been calculated. Note that, because of the grouping, this summarized dataset only has (30 subject * 6 activities) = 180 observations. Because only the columns containing a mean() or a standard deviation (std()) have been retained in the summarisation, the total number of columns in the summarised data set is 69 (subject code, activity description, activity code, plus 66 mean() of features).
+
+The additional variables contained in the three above mentioned data sets are:
+
+* sub_code - is the subject code i.e., the code of the person which the observation is related to, as derived from the "subject_test.txt" or "subject_train.txt" data files.
+
+* act_code - is the activity code, as derived from the "y_test.txt" or "y_train.txt" data files.
+
+* act_descr - is the activity description, as derived by looking up the activity code in the "activity_labels.txt" data file.
+
+For a description of the remaining variables, please refer to the rest of this document, after headline "Feature Selection".
+
+This is the end of modifications introduced by Flavio Massimo Saldicco.
+From now on, the file is the original one as distributed with the UCI HAR Dataset.
+=================
+
+
+Feature Selection 
+=================
+
+The features selected for this database come from the accelerometer and gyroscope 3-axial raw signals tAcc-XYZ and tGyro-XYZ. These time domain signals (prefix 't' to denote time) were captured at a constant rate of 50 Hz. Then they were filtered using a median filter and a 3rd order low pass Butterworth filter with a corner frequency of 20 Hz to remove noise. Similarly, the acceleration signal was then separated into body and gravity acceleration signals (tBodyAcc-XYZ and tGravityAcc-XYZ) using another low pass Butterworth filter with a corner frequency of 0.3 Hz. 
+
+Subsequently, the body linear acceleration and angular velocity were derived in time to obtain Jerk signals (tBodyAccJerk-XYZ and tBodyGyroJerk-XYZ). Also the magnitude of these three-dimensional signals were calculated using the Euclidean norm (tBodyAccMag, tGravityAccMag, tBodyAccJerkMag, tBodyGyroMag, tBodyGyroJerkMag). 
+
+Finally a Fast Fourier Transform (FFT) was applied to some of these signals producing fBodyAcc-XYZ, fBodyAccJerk-XYZ, fBodyGyro-XYZ, fBodyAccJerkMag, fBodyGyroMag, fBodyGyroJerkMag. (Note the 'f' to indicate frequency domain signals). 
+
+These signals were used to estimate variables of the feature vector for each pattern:  
+'-XYZ' is used to denote 3-axial signals in the X, Y and Z directions.
+
+tBodyAcc-XYZ
+tGravityAcc-XYZ
+tBodyAccJerk-XYZ
+tBodyGyro-XYZ
+tBodyGyroJerk-XYZ
+tBodyAccMag
+tGravityAccMag
+tBodyAccJerkMag
+tBodyGyroMag
+tBodyGyroJerkMag
+fBodyAcc-XYZ
+fBodyAccJerk-XYZ
+fBodyGyro-XYZ
+fBodyAccMag
+fBodyAccJerkMag
+fBodyGyroMag
+fBodyGyroJerkMag
+
+The set of variables that were estimated from these signals are: 
+
+mean(): Mean value
+std(): Standard deviation
+mad(): Median absolute deviation 
+max(): Largest value in array
+min(): Smallest value in array
+sma(): Signal magnitude area
+energy(): Energy measure. Sum of the squares divided by the number of values. 
+iqr(): Interquartile range 
+entropy(): Signal entropy
+arCoeff(): Autorregresion coefficients with Burg order equal to 4
+correlation(): correlation coefficient between two signals
+maxInds(): index of the frequency component with largest magnitude
+meanFreq(): Weighted average of the frequency components to obtain a mean frequency
+skewness(): skewness of the frequency domain signal 
+kurtosis(): kurtosis of the frequency domain signal 
+bandsEnergy(): Energy of a frequency interval within the 64 bins of the FFT of each window.
+angle(): Angle between to vectors.
+
+Additional vectors obtained by averaging the signals in a signal window sample. These are used on the angle() variable:
+
+gravityMean
+tBodyAccMean
+tBodyAccJerkMean
+tBodyGyroMean
+tBodyGyroJerkMean
+
+The complete list of variables of each feature vector is available in 'features.txt'
---
+++ run_analysis.R
@@ -0,0 +1,136 @@
+#####
+##### File Name:   run_analysis.R
+##### Last Edited: 24 Sep 2015
+##### Author:      Flavio Massimo Saldicco
+##### Purpose:     This is the script for Peer Assessment developed
+#####              in the context of the Getting and Cleaning Data Course
+#####              of Johns Hopkins Bloomberg School of Public Health,
+#####              hosted on the Coursera platform.
+#####              
+
+################################
+##### DOWNLOAD DATA FILE - START
+################################
+##### The following lines (up to the DOWNLOAD DATA FILE - END
+##### comment) are only needed to download and unzip the file
+##### assignment data files. These lines do not need
+##### to be executed if the data files have already
+##### been downloaded and unzipped manually.
+
+#set working directory (Note: change it appropriately)
+setwd("C:/Users/FlavioMassimo/Desktop/Getting and Cleaning")
+
+#download the data file
+url <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
+download.file(url, "dataset.zip", mode = "wb")
+list.files(".")
+
+#unzip the data file into the "UCI HAR Dataset" folder
+unzip("dataset.zip")
+##############################
+##### DOWNLOAD DATA FILE - END
+##############################
+
+#############################
+##### PERFORM TRANSFORMATIONS
+#############################
+#set working directory to the "UCI HAR Dataset" dataset one
+#Note: change it appropriately!
+setwd("C:/Users/FlavioMassimo/Desktop/Getting and Cleaning/UCI HAR Dataset")
+
+#read lookup table with the activity descriptions
+act_text <- read.table("activity_labels.txt", header=FALSE, col.names=c("act_code", "act_descr"))
+#read the lookup table with the variables descriptions
+feat_text <- read.table("features.txt", header=FALSE, col.names=c("feat_code", "feat_descr"))
+
+##### In this phase a tidy test dataset is created
+#read the subject test data
+tstSub <- read.table("test/subject_test.txt", header=FALSE, col.names=c("sub_code"))
+#read the activity (code) test data 
+tstAct <- read.table("test/y_test.txt", header=FALSE, col.names=c("act_code"))
+#read the test data
+tstDat <- read.table("test/X_test.txt", header=FALSE)
+
+#start bulding the tidy test dataset
+#1) bind columns subject and activity code
+tstComplete <- cbind(tstSub, tstAct)
+
+#plyr library is needed for next "join" command
+#Note: package plyr must be installed beforehand!
+library(plyr)
+
+#2) add column activity description (step 3 of the assignment)
+tstComplete <- join(tstComplete, act_text, by="act_code")
+
+#3) add data columns
+tstComplete <- cbind(tstComplete, tstDat)
+
+#now change the column names (step 4 of the assignment)
+colnames(tstComplete) <- c("sub_code", "act_code", "act_descr", as.character(feat_text$feat_descr))
+
+##### At this point we have a tidy dataset of the test data called tstComplete;
+##### the same transformations are now applied to the train dataset.
+
+##### In this phase a tidy train dataset is created
+#read the subject train data
+trnSub <- read.table("train/subject_train.txt", header=FALSE, col.names=c("sub_code"))
+#read the activity (code) train data 
+trnAct <- read.table("train/y_train.txt", header=FALSE, col.names=c("act_code"))
+#read the train data
+trnDat <- read.table("train/X_train.txt", header=FALSE)
+
+#start bulding the tidy train dataset
+#1) bind columns subject and activity code
+trnComplete <- cbind(trnSub, trnAct)
+
+#2) add column activity description (step 3 of the assignment)
+trnComplete <- join(trnComplete, act_text, by="act_code")
+
+#3) add data columns
+trnComplete <- cbind(trnComplete, trnDat)
+
+#now change the column names (step 4 of the assignment)
+colnames(trnComplete) <- c("sub_code", "act_code", "act_descr", as.character(feat_text$feat_descr))
+
+##### At this point we also have a tidy dataset of the train data called trnComplete.
+
+##### Let's now merge the test and train datasets (tstComplete and trnComplete)
+##### into the final dataset (step 1 of the assignment).
+finalDS <- rbind(tstComplete, trnComplete)
+
+#now we need to remove all columns which are not a mean nor a
+#standard deviation, based on the column name (step 2 of the assignment)
+for(i in 564:4) {
+    if(grepl("mean|std", colnames(finalDS[i])) == FALSE) {
+        finalDS[i] <- NULL
+    }
+}
+
+#the "meanFreq" columns are retained from the previous loop,
+#but they are not "mean" in a strict sense,
+#so now they are also eliminated
+for(i in 82:4) {
+    if(grepl("meanFreq", colnames(finalDS[i])) == TRUE) {
+        finalDS[i] <- NULL
+    }
+}
+
+##### Eventually, we get a tidy and full dataset including test
+##### and train data, ready to be used to derive a new dataset
+##### which summarise data (step 5 of the assignment).
+
+#dplyr library is needed for next "group_by/summarise" commands
+#Note: package dplyr must be installed beforehand!
+library("dplyr")
+
+#create a new dataset, grouped by subject and activity, where each
+#column is the mean() of the column itself
+sumDS <- finalDS %>% group_by(sub_code, act_descr) %>% summarise_each(funs(mean))
+
+#write the new dataset into a file for uploading onto Coursera
+write.table(sumDS, "sumDS.txt", row.names=FALSE)
+
+
+
+
+
